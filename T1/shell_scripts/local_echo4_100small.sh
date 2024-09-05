35076c47c8844883628d819eb7475b5b67efd06e
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-09-05 14:10:54 -0400

    Started Results.txt

--- T1/shell_scripts/local_echo4_100small.sh
+++ T1/shell_scripts/local_echo4_100small.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # Múltiples archivos pequeños en paralelo
-echo "One hundred small files in parallel for echo4 port 1819 size 1000:"
+echo "One hundred small files in parallel for echo4 port 1819 size 1000:" | tee ./times/real_time1.log
 for i in $(seq 1 100); do
   time ../client_bw.py 1000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_1000 &
+  { time ../client_bw.py 1000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_1000 ; } 2>> ./times/real_time1.log &
 done
 wait
-echo "One hundred small files in parallel for echo4 port 1819 size 2000:"
+echo "One hundred small files in parallel for echo4 port 1819 size 2000:" | tee ./times/real_time2.log
 for i in $(seq 1 100); do
   time ../client_bw.py 2000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_2000 &
+  { time ../client_bw.py 2000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_2000 ; } 2>> ./times/real_time2.log &
 done
 wait
-echo "One hundred small files in parallel echo4 port 1819 size 5000:"
+echo "One hundred small files in parallel echo4 port 1819 size 5000:" | tee ./times/real_time5.log
 for i in $(seq 1 100); do
   time ../client_bw.py 5000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_5000 &
+  { time ../client_bw.py 5000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_5000 ; } 2>> ./times/real_time5.log &
 done
 wait
-echo "One hundred small files in parallel echo4 port 1819 size 8000:"
+echo "One hundred small files in parallel echo4 port 1819 size 8000:" | tee ./times/real_time8.log
 for i in $(seq 1 100); do
   time ../client_bw.py 8000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_8000 &
+  { time ../client_bw.py 8000 localhost 1819 < ./small_files/file${i}.bin > ./trash/OUT100_${i}_8000 ; } 2>> ./times/real_time8.log &
 done
 wait
