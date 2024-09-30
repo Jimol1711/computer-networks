dd1f36553c0e70ba692a2a878dd74ae6c53d3ed4
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-09-29 23:06:45 -0300

    preparing files for T2

---
+++ T2/jsockets-py/server_echo_udp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Echo server UDP program - version of server_echo_udp.c, mono-cliente
+import jsockets
+
+s = jsockets.socket_udp_bind(1818)
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+print('Server is running on port 1818')
+
+while True:
+    data, addr = s.recvfrom(1024)
+    if not data: break
+    s.sendto(data, addr)
+
+s.close()
